a9b966e0cc993aa35058d52609c41132f77842b5
Lily Scott <me@suchipi.com>
2019-12-21 17:46:08 -0700

    Add webpack builder

---
+++ packages/webpack/README.md
@@ -0,0 +1,9 @@
+# `@workspace-builder/webpack`
+
+This is a workspace builder module for [`workspace-builder`](http://npm.im/workspace-builder).
+
+It runs webpack in the workspace, using `webpack.config.js` in the workspace root if present, or the monorepo root if not. If no `webpack.config.js` is present, it uses webpack's default config.
+
+## License
+
+MIT
---
+++ packages/webpack/index.js
@@ -0,0 +1,42 @@
+const fs = require("fs");
+const {
+  bin,
+  rootDir,
+  workspaceDir,
+  exec,
+} = require("@workspace-builder/tools");
+
+module.exports = function build(workspace, options) {
+  let webpackConfigPath = null;
+  if (fs.existsSync(rootDir("webpack.config.js"))) {
+    webpackConfigPath = rootDir("webpack.config.js");
+  }
+  if (fs.existsSync(workspaceDir("webpack.config.js"))) {
+    webpackConfigPath = workspaceDir("webpack.config.js");
+  }
+
+  const env =
+    process.env.NODE_ENV || options.watch ? "development" : "production";
+
+  let hasModeSet = true;
+  if (webpackConfigPath) {
+    try {
+      let webpackConfig = require(webpackConfigPath);
+      if (typeof webpackConfig === "function") {
+        webpackConfig = webpackConfig(env);
+      }
+      if (!Array.isArray(webpackConfig)) {
+        const mode = webpackConfig.mode;
+        hasModeSet = Boolean(mode);
+      }
+    } catch (err) {}
+  }
+
+  exec(
+    `env NODE_ENV="${env}" ${bin("webpack")} ${
+      hasModeSet ? "" : `--mode ${env}`
+    } ${options.watch ? "--watch" : ""} ${
+      webpackConfigPath ? `--config ${webpackConfigPath}` : ""
+    }`
+  );
+};
